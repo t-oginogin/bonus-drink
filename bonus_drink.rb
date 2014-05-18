9c355abff1b9e56832ed0777e6d1c88176b1d3e2
Tatsuya Ogi <t.oginogin@gmail.com>
2014-05-18 17:26:56 +0900

    to private 'bonus' method

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,14 +1,17 @@
 class BonusDrink
   BONUS_COUNT = 3.freeze
 
-  def self.total_count_for(amount)
-    amount + self.bonus(amount)
-  end
+  class << self
+    def total_count_for(amount)
+      amount + bonus(amount)
+    end
 
-  def self.bonus(amount)
-    bonus_amount = (amount / BONUS_COUNT).floor
-    rest = (amount % BONUS_COUNT) + bonus_amount
-    bonus_amount += self.bonus(rest) if rest >= BONUS_COUNT
-    bonus_amount
+    private
+    def bonus(amount)
+      bonus_amount = (amount / BONUS_COUNT).floor
+      rest = (amount % BONUS_COUNT) + bonus_amount
+      bonus_amount += bonus(rest) if rest >= BONUS_COUNT
+      bonus_amount
+    end
   end
 end
