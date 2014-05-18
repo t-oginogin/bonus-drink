481904449f232df96ee9207e840ed5cb9d7c384d
Tatsuya Ogi <t.oginogin@gmail.com>
2014-05-18 17:10:29 +0900

    first answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
+  BONUS_COUNT = 3.freeze
+
   def self.total_count_for(amount)
-    0
+    amount + self.bonus(amount)
+  end
+
+  def self.bonus(amount)
+    bonus_amount = (amount / BONUS_COUNT).floor
+    rest = (amount % BONUS_COUNT) + bonus_amount
+    bonus_amount += self.bonus(rest) if rest >= BONUS_COUNT
+    bonus_amount
   end
 end
